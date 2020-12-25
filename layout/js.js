c6174b72b2f4b50f5283f36ede497f6fbd3d4dea
Solovejko <solovejko00@mail.ru>
2020-12-25 21:40:00 +0300

    8 lab

---
+++ layout/js.js
@@ -0,0 +1,12 @@
+function ff()
+{
+    t = document.getElementById("index.html#windMain");
+    t.setAttribute("id", "123");
+    t.style.display = "none";
+    t = document.getElementById("windBack");
+    t.setAttribute("id", "1234");
+    t.style.display = "none";
+    t = document.getElementById("wind");
+    t.setAttribute("id", "12345");
+    t.style.display = "none";
+}
